7eb22296e38131974d7ae4865d8abbbe77b7ce05
GalenReed <galenreed@vt.edu>
2025-03-26 17:48:15 -0400

    yay!

--- server/app.py
+++ server/app.py
@@ -21,7 +21,35 @@ class Plants(Resource):
 
 class PlantByID(Resource):
     pass
-        
+
+@app.route('/plants')
+def index_plants():
+    plants = [plant.to_dict() for plant in Plant.query.all()]
+
+    return make_response(plants, 200)
+
+@app.route('/plants/<int:id>')
+def show_plants(id):
+    plant = db.session.get(Plant, id)
+
+    plant_dict = plant.to_dict()
+
+    return make_response(plant_dict, 200)
+
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+
+    new_plant = Plant(
+        name = data['name'],
+        image = data['image'],
+        price = data['price']
+        )
+
+    db.session.add(new_plant)
+    db.session.commit()
+
+    return make_response(new_plant.to_dict(), 201)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
